96e1097481d13e9dd5edbca24e50bd92211c32b0
Thomas Ha <thomasha.1310@gmail.com>
2025-02-08 17:42:34 -0500

    Bugfixes

--- src/main/java/com/thomasha/Lox.java
+++ src/main/java/com/thomasha/Lox.java
@@ -61,7 +61,7 @@ public class Lox {
     }
 
     private static void report(int line, String where, String message) {
-        System.err.println("[line" + line + "] Error" + where + ": " + message);
+        System.err.println("[line " + line + "] Error" + where + ": " + message);
         hadError = true;
     }
 }
--- src/main/java/com/thomasha/Scanner.java
+++ src/main/java/com/thomasha/Scanner.java
@@ -105,7 +105,7 @@ public class Scanner {
             case '<':
                 addToken(match('=') ? LESS_EQUAL : LESS);
                 break;
-            case '?':
+            case '>':
                 addToken(match('=') ? GREATER_EQUAL : GREATER);
                 break;
 
