fcbedaef7c8089540d914fced750cc620602ad05
Thomas Ha <thomasha.1310@gmail.com>
2025-02-08 17:23:34 -0500

    Updated Scanner.java, pom.xml

--- lox-interpreter/src/main/java/com/thomasha/Scanner.java
+++ lox-interpreter/src/main/java/com/thomasha/Scanner.java
@@ -8,19 +8,41 @@ import java.util.Map;
 import static com.thomasha.TokenType.*;
 
 public class Scanner {
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("for", FOR);
+        keywords.put("fun", FUN);
+        keywords.put("if", IF);
+        keywords.put("nil", NIL);
+        keywords.put("or", OR);
+        keywords.put("print", PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super", SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while", WHILE);
+    }
+
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
 
     // First character in the lexeme being scanned; index is absolute with respect
-    // to `source`. That is, if `start` is 5, then the first character of the lexeme
-    // is at source.charAt(5).
+    // to source. That is, if start is 5, then the first character of the lexeme
+    // is source.charAt(5).
     private int start = 0;
 
     // Character in the lexeme that is currently being considered; index is absolute
-    // with respect to `source`.
+    // with respect to source.
     private int current = 0;
 
-    // Traces which source line `current` is on to create tokens that know their
+    // Traces which source line current is on to create tokens that know their
     // location (for error reporting).
     private int line = 1;
 
@@ -107,10 +129,67 @@ public class Scanner {
                 line++;
                 break;
 
+            case '"':
+                string();
+                break;
+
             default:
-                Lox.error(line, "Unexpected character.");
+                if (isDigit(c)) {
+                    number();
+                } else if (isAlpha(c)) {
+                    identifier();
+                } else {
+                    Lox.error(line, "Unexpected character.");
+                }
                 break;
         }
+    }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek()))
+            advance();
+
+        String text = source.substring(start, current);
+        // If the text does not match a keyword, it is an identifier.
+        TokenType type = keywords.get(text);
+        if (type == null)
+            type = IDENTIFIER;
+        addToken(type);
+    }
+
+    private void number() {
+        // Advance until a non-digit (0-9) character is found.
+        while (isDigit(peek()))
+            advance();
+
+        // Check if there is a fractional part.
+        if (peek() == '.' && isDigit(peekNext())) {
+            advance();
+            // Continue advancing until the end of the fractional part is reached;
+            while (isDigit(peek()))
+                advance();
+        }
+
+        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n')
+                line++;
+            advance();
+        }
+
+        if (isAtEnd()) {
+            Lox.error(line, "Unterminated string.");
+            return;
+        }
+
+        advance();
+
+        // Trim surrounding quotation marks.
+        String value = source.substring(start + 1, current - 1);
+        addToken(STRING, value);
     }
 
     private boolean match(char expected) {
@@ -122,17 +201,37 @@ public class Scanner {
         return true;
     }
 
+    // Returns the value of source at current index without advancing.
     private char peek() {
         if (isAtEnd())
             return '\0';
         return source.charAt(current);
     }
 
+    // Returns the value of source at one past the current index without advancing.
+    private char peekNext() {
+        if (current + 1 >= source.length())
+            return '\0';
+        return source.charAt(current + 1);
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
     private boolean isAtEnd() {
         return current >= source.length();
     }
 
-    // Reads and returns the current character, then moves `current` forward.
+    // Reads and returns the current character, then moves current forward.
     private char advance() {
         return source.charAt(current++);
     }
