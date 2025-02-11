c04aba0e71d03bfc8db44c7683579b0bb1985351
Thomas Ha <thomasha.1310@gmail.com>
2025-02-10 21:20:46 -0500

    Create RuntimeError.java

--- src/main/java/com/thomasha/Interpreter.java
+++ src/main/java/com/thomasha/Interpreter.java
@@ -35,6 +35,12 @@ public class Interpreter implements Expr.Visitor<Object> {
         throw new RuntimeError(operator, "Operand must be a number.");
     }
 
+    private void checkNumberOperands(Token operator, Object left, Object right) {
+        if (left instanceof Double && right instanceof Double)
+            return;
+        throw new RuntimeError(operator, "Operands must be numbers.");
+    }
+
     // Returns false if and only if the object is null (nil) or is a boolean value
     // that evaluates to false. Otherwise, returns true (including if the object is
     // an integer equal to 0).
@@ -75,28 +81,33 @@ public class Interpreter implements Expr.Visitor<Object> {
             case EQUAL_EQUAL:
                 return isEqual(left, right);
             case GREATER:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left > (double) right;
             case GREATER_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left >= (double) right;
             case LESS:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left < (double) right;
             case LESS_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left <= (double) right;
             case MINUS:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left - (double) right;
             case PLUS:
                 if (left instanceof Double && right instanceof Double) {
                     return (double) left + (double) right;
-                }
-
-                if (left instanceof String && right instanceof String) {
+                } else if (left instanceof String && right instanceof String) {
                     return (String) left + (String) right;
+                } else {
+                    throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings.");
                 }
-
-                break;
             case SLASH:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left / (double) right;
             case STAR:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left * (double) right;
 
         }
