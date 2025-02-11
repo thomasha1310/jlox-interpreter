2dd634c8db325f61c644fba48a37a84b8b728487
Thomas Ha <thomasha.1310@gmail.com>
2025-02-10 21:15:36 -0500

    Create Interpreter.java

---
+++ src/main/java/com/thomasha/Interpreter.java
@@ -0,0 +1,107 @@
+package com.thomasha;
+
+public class Interpreter implements Expr.Visitor<Object> {
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    }
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        // Evaluates the inside part of the grouping, essentially removing the left and
+        // right parentheses.
+        return evaluate(expr.expression);
+    }
+
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case MINUS:
+                checkNumberOperand(expr.operator, right);
+                return -(double) right;
+            case BANG:
+                return !isTruthy(right);
+            // Unreachable.
+            default:
+                return null;
+        }
+    }
+
+    private void checkNumberOperand(Token operator, Object operand) {
+        if (operand instanceof Double)
+            return;
+        throw new RuntimeError(operator, "Operand must be a number.");
+    }
+
+    // Returns false if and only if the object is null (nil) or is a boolean value
+    // that evaluates to false. Otherwise, returns true (including if the object is
+    // an integer equal to 0).
+    private boolean isTruthy(Object object) {
+        if (object == null)
+            return false;
+        if (object instanceof Boolean)
+            return (boolean) object;
+        return true;
+    }
+
+    // Returns false if one object is null and the other is not and returns true if
+    // both objects are null. Otherwise, defers to the Java Object's equals() method
+    // for String, Double, and Boolean comparisons.
+    private boolean isEqual(Object a, Object b) {
+        if (a == null && b == null)
+            return true;
+        if (a == null)
+            return false;
+
+        return a.equals(b);
+    }
+
+    // Evaluates the expression by having it accept the Visitor. The expression then
+    // calls the corresponding visit method in the visiting Interpreter.
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case BANG_EQUAL:
+                return !isEqual(left, right);
+            case EQUAL_EQUAL:
+                return isEqual(left, right);
+            case GREATER:
+                return (double) left > (double) right;
+            case GREATER_EQUAL:
+                return (double) left >= (double) right;
+            case LESS:
+                return (double) left < (double) right;
+            case LESS_EQUAL:
+                return (double) left <= (double) right;
+            case MINUS:
+                return (double) left - (double) right;
+            case PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                    return (double) left + (double) right;
+                }
+
+                if (left instanceof String && right instanceof String) {
+                    return (String) left + (String) right;
+                }
+
+                break;
+            case SLASH:
+                return (double) left / (double) right;
+            case STAR:
+                return (double) left * (double) right;
+
+        }
+
+        // Unreachable.
+        return null;
+    }
+}
