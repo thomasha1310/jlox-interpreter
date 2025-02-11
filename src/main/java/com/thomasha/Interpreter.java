23e907f2da3cadcb72b6479954823456da14bf5c
Thomas Ha <thomasha.1310@gmail.com>
2025-02-10 21:28:51 -0500

    Complete Interpreter.java

--- src/main/java/com/thomasha/Interpreter.java
+++ src/main/java/com/thomasha/Interpreter.java
@@ -1,6 +1,15 @@
 package com.thomasha;
 
 public class Interpreter implements Expr.Visitor<Object> {
+    void interpret(Expr expression) {
+        try {
+            Object value = evaluate(expression);
+            System.out.println(stringify(value));
+        } catch (RuntimeException error) {
+            Lox.runtimeError(error);
+        }
+    }
+
     @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
         return expr.value;
@@ -62,6 +71,21 @@ public class Interpreter implements Expr.Visitor<Object> {
             return false;
 
         return a.equals(b);
+    }
+
+    private String stringify(Object object) {
+        if (object == null)
+            return "nil";
+
+        if (object instanceof Double) {
+            String text = object.toString();
+            // Handles numbers with integer values.
+            if (text.endsWith(".0"))
+                text = text.substring(0, text.length() - 2);
+            return text;
+        }
+
+        return object.toString();
     }
 
     // Evaluates the expression by having it accept the Visitor. The expression then
