94a133988d1728987beac44e99f912e35f28947b
Thomas Ha <thomasha.1310@gmail.com>
2025-02-11 20:39:24 -0500

    Update Interpreter.java and Lox.java

--- src/main/java/com/thomasha/Interpreter.java
+++ src/main/java/com/thomasha/Interpreter.java
@@ -1,10 +1,13 @@
 package com.thomasha;
 
-public class Interpreter implements Expr.Visitor<Object> {
-    void interpret(Expr expression) {
+import java.util.List;
+
+public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+    void interpret(List<Stmt> statements) {
         try {
-            Object value = evaluate(expression);
-            System.out.println(stringify(value));
+            for (Stmt statement : statements) {
+                execute(statement);
+            }
         } catch (RuntimeError error) {
             Lox.runtimeError(error);
         }
@@ -92,6 +95,23 @@ public class Interpreter implements Expr.Visitor<Object> {
     // calls the corresponding visit method in the visiting Interpreter.
     private Object evaluate(Expr expr) {
         return expr.accept(this);
+    }
+
+    private void execute(Stmt stmt) {
+        stmt.accept(this);
+    }
+
+    @Override
+    public Void visitExpressionStmt(Stmt.Expression stmt) {
+        evaluate(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitPrintStmt(Stmt.Print stmt) {
+        Object value = evaluate(stmt.expression);
+        System.out.println(stringify(value));
+        return null;
     }
 
     @Override
