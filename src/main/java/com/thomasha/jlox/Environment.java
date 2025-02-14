3337a5461acfcd63e4f95c2c1df9c11d5dfca763
Thomas Ha <thomasha.1310@gmail.com>
2025-02-14 18:08:56 -0500

    Update Environment.java with scope support

--- src/main/java/com/thomasha/jlox/Environment.java
+++ src/main/java/com/thomasha/jlox/Environment.java
@@ -4,13 +4,31 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Environment {
+    final Environment enclosing;
     private final Map<String, Object> values = new HashMap<>();
+
+    // Environment initializer for global scope environment.
+    Environment() {
+        enclosing = null;
+    }
+
+    // Environment initializer for local scope environments.
+    Environment(Environment enclosing) {
+        this.enclosing = enclosing;
+    }
 
     // Provided the name of a variable, returns the value associated with that
     // variable. If the variable has not been defined, throws a RuntimeError.
     Object get(Token name) {
         if (values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
+        }
+
+        // If the variable is not found in the current scope, check the enclosing scope.
+        // The process continues until the variable is found or the global scope is
+        // reached (at which point a RuntimeError is thrown).
+        if (enclosing != null) {
+            return enclosing.get(name);
         }
 
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
@@ -26,8 +44,17 @@ public class Environment {
     // already been defined. If the variable has not been defined, throws a
     // RuntimeError, similar to get().
     void assign(Token name, Object value) {
+        // If the variable is in the current scope, assign it and exit.
         if (values.containsKey(name.lexeme)) {
             values.put(name.lexeme, value);
+            return;
+        }
+
+        // If the variable is not found in the current scope, check the enclosing scope.
+        // The process continues until the variable is found or the global scope is
+        // reached (at which point a RuntimeError is thrown).
+        if (enclosing != null) {
+            enclosing.assign(name, value);
             return;
         }
 
