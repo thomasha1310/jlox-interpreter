2f3142ff3f51295275a8e3d3e0ec9c0c76ebbbb4
Thomas Ha <thomasha.1310@gmail.com>
2025-02-14 21:29:58 -0500

    Update Environment.java with Javadoc

--- src/main/java/com/thomasha/jlox/Environment.java
+++ src/main/java/com/thomasha/jlox/Environment.java
@@ -7,18 +7,31 @@ public class Environment {
     final Environment enclosing;
     private final Map<String, Object> values = new HashMap<>();
 
-    // Environment initializer for global scope environment.
+    /**
+     * Initializes an Environment object for the global scope environment.
+     */
     Environment() {
         enclosing = null;
     }
 
-    // Environment initializer for local scope environments.
+    /**
+     * Initializes an Environment object for a local scope environment.
+     * 
+     * @param enclosing the {@code environment} that encloses the new
+     *                  {@code environment}
+     */
     Environment(Environment enclosing) {
         this.enclosing = enclosing;
     }
 
-    // Provided the name of a variable, returns the value associated with that
-    // variable. If the variable has not been defined, throws a RuntimeError.
+    /**
+     * Returns the value associated with the given variable name. Throws a
+     * {@code RuntimeError} if the variable has not been defined.
+     * 
+     * @param name the variable to retrieve the value of
+     * @return the value associated with the variable
+     * @throws RuntimeError if the specified variable has not been defined
+     */
     Object get(Token name) {
         if (values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
@@ -34,15 +47,26 @@ public class Environment {
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
 
-    // Sets the value of the specified variable. If the specified variable has
-    // already been defined, simply override the old value.
+    /**
+     * Sets the value of the specified variable, overriding the previous value if
+     * one existed.
+     * 
+     * @param name  the name of the variable
+     * @param value the value to be associated with the variable
+     */
     void define(String name, Object value) {
         values.put(name, value);
     }
 
-    // Sets the value of the specified variable if and only if the variable has
-    // already been defined. If the variable has not been defined, throws a
-    // RuntimeError, similar to get().
+    /**
+     * Sets the value of the specified variable if and only if the variable has
+     * already been defined. If the variable has not been defined, throws a
+     * {@code RuntimeError}, similar to {@link #get}.
+     * 
+     * @param name  the variable to assign a value to
+     * @param value the value to assign to the specified variable
+     * @throws RuntimeError if the specified variable has not been defined
+     */
     void assign(Token name, Object value) {
         // If the variable is in the current scope, assign it and exit.
         if (values.containsKey(name.lexeme)) {
