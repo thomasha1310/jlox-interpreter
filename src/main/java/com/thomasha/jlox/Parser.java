9fed960b227799da3dad8b23190e25a7528d728c
Thomas Ha <thomasha.1310@gmail.com>
2025-02-16 09:36:43 -0500

    Update Parser to support parsing while statements

--- src/main/java/com/thomasha/jlox/Parser.java
+++ src/main/java/com/thomasha/jlox/Parser.java
@@ -49,6 +49,9 @@ public class Parser {
         if (match(PRINT)) {
             return printStatement();
         }
+        if (match(WHILE)) {
+            return whileStatement();
+        }
         if (match(LEFT_BRACE)) {
             return new Stmt.Block(block());
         }
@@ -77,6 +80,15 @@ public class Parser {
         Expr value = expression();
         consume(SEMICOLON, "Expect ';' after value.");
         return new Stmt.Print(value);
+    }
+
+    private Stmt whileStatement() {
+        consume(LEFT_PAREN, "Expect '(' after 'while'.");
+        Expr condition = expression();
+        consume(RIGHT_PAREN, "Expect ')' after condition.");
+        Stmt body = statement();
+
+        return new Stmt.While(condition, body);
     }
 
     private Stmt varDeclaration() {
