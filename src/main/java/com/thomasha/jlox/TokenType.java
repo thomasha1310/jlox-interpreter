a9c916d545473c4f51655291ff14ea027c81b24f
Thomas Ha <thomasha.1310@gmail.com>
2025-02-19 10:25:27 -0500

    Add additional operators to TokenType
    Added PERCENT and compound assignment operators for future implementation

--- src/main/java/com/thomasha/jlox/TokenType.java
+++ src/main/java/com/thomasha/jlox/TokenType.java
@@ -4,12 +4,20 @@ enum TokenType {
     // Single-character tokens.
     LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
     COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+    PERCENT,
 
     // One or two character tokens.
     BANG, BANG_EQUAL,
     EQUAL, EQUAL_EQUAL,
     GREATER, GREATER_EQUAL,
     LESS, LESS_EQUAL,
+
+    // Compound assignment operators.
+    PLUS_EQUAL,
+    MINUS_EQUAL,
+    STAR_EQUAL,
+    SLASH_EQUAL,
+    PERCENT_EQUAL,
 
     // Literals.
     IDENTIFIER, STRING, NUMBER,
