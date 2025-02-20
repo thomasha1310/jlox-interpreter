c621ef6686d698e2a5067ed8886b2f893f6a78d0
Thomas Ha <thomasha.1310@gmail.com>
2025-02-20 15:46:46 -0500

    Fix unnecessary import

--- src/main/java/com/thomasha/jlox/Interpreter.java
+++ src/main/java/com/thomasha/jlox/Interpreter.java
@@ -3,8 +3,6 @@ package com.thomasha.jlox;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.thomasha.jlox.Stmt.Function;
-
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     final Environment globals = new Environment();
     private Environment environment = globals;
@@ -229,7 +227,7 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     }
 
     @Override
-    public Void visitFunctionStmt(Function stmt) {
+    public Void visitFunctionStmt(Stmt.Function stmt) {
         LoxFunction function = new LoxFunction(stmt);
         environment.define(stmt.name.lexeme, function);
         return null;
