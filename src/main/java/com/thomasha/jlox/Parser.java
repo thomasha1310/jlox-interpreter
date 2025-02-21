307fe7bc3b726d8dccfc63fec0b58ef7c1532d4a
Thomas Ha <thomasha.1310@gmail.com>
2025-02-21 10:19:20 -0500

    Update Parser to support return stmt

--- src/main/java/com/thomasha/jlox/Parser.java
+++ src/main/java/com/thomasha/jlox/Parser.java
@@ -77,6 +77,9 @@ public class Parser {
         if (match(PRINT)) {
             return printStatement();
         }
+        if (match(RETURN)) {
+            return returnStatement();
+        }
         if (match(WHILE)) {
             return whileStatement();
         }
@@ -164,6 +167,17 @@ public class Parser {
         Expr value = expression();
         consume(SEMICOLON, "Expect ';' after value.");
         return new Stmt.Print(value);
+    }
+
+    private Stmt returnStatement() {
+        Token keyword = previous();
+        Expr value = null;
+        if (!check(SEMICOLON)) {
+            value = expression();
+        }
+
+        consume(SEMICOLON, "Expect ';' after return value.");
+        return new Stmt.Return(keyword, value);
     }
 
     private Stmt whileStatement() {
