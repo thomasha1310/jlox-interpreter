a47a19820ca2bfe7d00faf5044fee60eb238295b
Thomas Ha <thomasha.1310@gmail.com>
2025-02-23 14:47:08 -0500

    Update Lox.java to include Resolver

--- src/main/java/com/thomasha/jlox/Lox.java
+++ src/main/java/com/thomasha/jlox/Lox.java
@@ -61,6 +61,9 @@ public class Lox {
         if (hadError)
             return;
 
+        Resolver resolver = new Resolver(interpreter);
+        resolver.resolve(statements);
+
         interpreter.interpret(statements);
     }
 
