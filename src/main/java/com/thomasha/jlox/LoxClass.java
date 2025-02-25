acf7d2a41ecb9b2a0f89396f01700a15e58388a4
Thomas Ha <thomasha.1310@gmail.com>
2025-02-24 20:15:18 -0500

    Update LoxClass to be callable for instantiation

--- src/main/java/com/thomasha/jlox/LoxClass.java
+++ src/main/java/com/thomasha/jlox/LoxClass.java
@@ -1,6 +1,6 @@
 package com.thomasha.jlox;
 
-public class LoxClass {
+public class LoxClass implements LoxCallable {
     final String name;
 
     LoxClass(String name) {
@@ -10,5 +10,16 @@ public class LoxClass {
     @Override
     public String toString() {
         return name;
+    }
+
+    @Override
+    public Object call(Interpreter interpreter, List<Object> arguments) {
+        LoxInstance instance = new LoxInstance(this);
+        return instance;
+    }
+
+    @Override
+    public int arity() {
+        return 0;
     }
 }
