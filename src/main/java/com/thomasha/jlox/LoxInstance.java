b8f5d23735f9370d224070c7810863c320dfb994
Thomas Ha <thomasha.1310@gmail.com>
2025-02-25 20:49:22 -0500

    Add get method to LoxInstance

--- src/main/java/com/thomasha/jlox/LoxInstance.java
+++ src/main/java/com/thomasha/jlox/LoxInstance.java
@@ -1,10 +1,22 @@
 package com.thomasha.jlox;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoxInstance {
     private LoxClass loxClass;
+    private final Map<String, Object> fields = new HashMap<>();
 
     LoxInstance(LoxClass loxClass) {
         this.loxClass = loxClass;
+    }
+
+    Object get(Token name) {
+        if (fields.containsKey(name.lexeme)) {
+            return fields.get(name.lexeme);
+        }
+
+        throw new RuntimeError(name, "Undefined property '" + name.lexeme + "'.");
     }
 
     @Override
