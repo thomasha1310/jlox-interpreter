211ad05560dc131311be58aedfe3f22fea81ff11
Thomas Ha <thomasha.1310@gmail.com>
2025-02-25 20:49:32 -0500

    Add visitGetExpr method to Interpreter

--- src/main/java/com/thomasha/jlox/Interpreter.java
+++ src/main/java/com/thomasha/jlox/Interpreter.java
@@ -207,6 +207,16 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     }
 
     @Override
+    public Object visitGetExpr(Expr.Get expr) {
+        Object object = evaluate(expr.object);
+        if (object instanceof LoxInstance) {
+            return ((LoxInstance) object).get(expr.name);
+        }
+
+        throw new RuntimeError(expr.name, "Only instances have properties.");
+    }
+
+    @Override
     public Object visitAssignExpr(Expr.Assign expr) {
         Object value = evaluate(expr.value);
 
