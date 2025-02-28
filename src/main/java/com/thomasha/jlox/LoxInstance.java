47c306bb170cb83fa26016f777de9289c2f46b57
Thomas Ha <thomasha.1310@gmail.com>
2025-02-27 19:23:05 -0500

    Update LoxInstance get method to support methods

--- src/main/java/com/thomasha/jlox/LoxInstance.java
+++ src/main/java/com/thomasha/jlox/LoxInstance.java
@@ -16,6 +16,11 @@ public class LoxInstance {
             return fields.get(name.lexeme);
         }
 
+        LoxFunction method = loxClass.findMethod(name.lexeme);
+        if (method != null) {
+            return method;
+        }
+
         throw new RuntimeError(name, "Undefined property '" + name.lexeme + "'.");
     }
 
